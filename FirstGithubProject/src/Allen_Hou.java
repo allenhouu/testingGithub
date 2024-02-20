a096811194439bb5c01a3a4bdff243ed77528540
matthewkouveliotes <160647852+matthewkouveliotes@users.noreply.github.com>
2024-02-20 13:56:29 -0500

    Hello Allen & Jared

--- FirstGithubProject/src/Allen_Hou.java
+++ FirstGithubProject/src/Allen_Hou.java
@@ -1,6 +1,7 @@
 public class Allen_Hou {
     public static void main(String[] args) {
         System.out.println("Hello Allen");
+        System.out.println("Hello Allen");
         System.out.println("Hello other editor: Jared");
     }
 }
